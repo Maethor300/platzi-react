d1c3f31fac255b9ae82f11843272ad1200b1ec84
David Mendoza Erazo <davidleomendoza300@gmail.com>
2023-12-19 11:54:06 -0500

    TodoItem

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@
 import './App.css';
 
 function App() {
+  return(
+    <div>
+      
+    <TodoItem/>
+    <TodoItem/>
+    <TodoItem/>
+    </div>
+  )
+}
+function TodoItem(){
   return (
     <div className="App">
-        <span>V</span>
-        <p>Estudiar programacion</p>
-        <span>X</span>
+      <span>V</span>
+      <p>Leer Lord of the rings</p>
+      <span>X</span>
     </div>
   );
 }
-
 export default App;
